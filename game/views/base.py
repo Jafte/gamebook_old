fd1d304861890f910d7e124464fdf5341a2f1caf
Jafte <jafte@conomy.ru>
2018-01-19 20:48:03 +0300

    some

--- game/views/base.py
+++ game/views/base.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import ContextMixin
-from game.models import Game
+from game.models import Game, Scene
 
 
 class GameMixin(ContextMixin):
@@ -13,24 +13,83 @@ class GameMixin(ContextMixin):
 
     def get_context_data(self, **kwargs):
         context = {
-            'game': self.game
+            'game': self.get_game()
         }
         context.update(kwargs)
         return super().get_context_data(**context)
 
     def get_game(self):
-        queryset = Game.objects.filter(author=self.request.user)
+        if not self.game:
+            queryset = Game.objects.filter(author=self.request.user)
 
-        pk = self.kwargs.get(self.game_pk_url_kwarg, None)
-        queryset = queryset.filter(pk=pk)
+            pk = self.kwargs.get(self.game_pk_url_kwarg, None)
+            queryset = queryset.filter(pk=pk)
 
-        try:
-            # Get the single item from the filtered queryset
-            obj = queryset.get()
-        except queryset.model.DoesNotExist:
-            raise Http404(_("No %(verbose_name)s found matching the query") %
-                          {'verbose_name': queryset.model._meta.verbose_name})
-        return obj
+            try:
+                # Get the single item from the filtered queryset
+                self.game = queryset.get()
+            except queryset.model.DoesNotExist:
+                raise Http404(_("No %(verbose_name)s found matching the query") %
+                            {'verbose_name': queryset.model._meta.verbose_name})
+
+        return self.game
+
+
+class DetailWithGameView(GameMixin, DetailView):
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+class DeleteWithGameView(DetailWithGameView, DeleteView):
+    def get_success_url(self):
+        game = self.get_game()
+        return game.get_absolute_url()
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class CreateWithGameView(GameMixin, CreateView):
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class UpdateWithGameView(GameMixin, UpdateView):
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class SceneGameMixin(GameMixin):
+    scene_pk_url_kwarg = 'scene_pk'
+    scene = None
+
+    def get_context_data(self, **kwargs):
+        context = {
+            'scene': self.get_scene()
+        }
+        context.update(kwargs)
+        return super().get_context_data(**context)
+
+    def get_scene(self):
+        if not self.scene
+            queryset = Scene.objects.filter(game=self.get_game())
+
+            pk = self.kwargs.get(self.game_pk_url_kwarg, None)
+            queryset = queryset.filter(pk=pk)
+
+            try:
+                # Get the single item from the filtered queryset
+                obj = queryset.get()
+            except queryset.model.DoesNotExist:
+                raise Http404(_("No %(verbose_name)s found matching the query") %
+                            {'verbose_name': queryset.model._meta.verbose_name})
+        return self.scene
 
 
 class DetailWithGameView(GameMixin, DetailView):
