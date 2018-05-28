af5c7d861aa971920a82fc8b55855585b8f4b85c
Jafte <jafte@conomy.ru>
2018-05-28 19:36:38 +0300

    fix some shit

--- game/views/game.py
+++ game/views/game.py
@@ -1,12 +1,9 @@
 from django.urls import reverse
-from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from game.models import Game
-from play.models import Session
-from telegrambot.models import User
 
 
 class GameListView(LoginRequiredMixin, ListView):
@@ -22,39 +19,6 @@ class GameDetailView(LoginRequiredMixin, DetailView):
     template_name = 'game/detail.html'
     model = Game
     pk_url_kwarg = 'game_pk'
-
-
-class GamePlayView(GameDetailView):
-    template_name = 'game/play.html'
-    game_session = None
-
-    def get_session(self):
-        if not self.game_session:
-            self.game_session = self.object.users_sessions.filter(
-                user=self.request.user,
-                status=Session.STATUS_ACTIVE
-            )
-
-        return self.game_session
-
-    def get_context_data(self, **kwargs):
-        context = {
-            'session': self.get_session()
-        }
-        context.update(kwargs)
-        return super().get_context_data(**context)
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-
-        action = self.request.POST.get('action', False)
-        if action == 'new_game':
-            session = self.get_session()
-            if session:
-                session.finish_game()
-
-        context = self.get_context_data(object=self.object)
-        return self.render_to_response(context)
 
 
 class GameCreateView(LoginRequiredMixin, CreateView):
--- play/views.py
+++ play/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from play.models import Session
+from game.views.game import GameDetailView
 
-# Create your views here.
+
+class PlayView(GameDetailView):
+    template_name = 'play/detail.html'
+    game_session = None
+
+    def get_session(self):
+        if not self.game_session:
+            self.game_session = self.object.get_user_game(self.request.user)
+
+        return self.game_session
+
+    def get_context_data(self, **kwargs):
+        context = {
+            'session': self.get_session()
+        }
+        context.update(kwargs)
+        return super().get_context_data(**context)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        action = self.request.POST.get('action', False)
+        if action == 'new_game':
+            pass
+
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
